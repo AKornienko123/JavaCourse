673754e768df581204f681860192cf8d37a57c39
AKornienko123 <71881413+AKornienko123@users.noreply.github.com>
2021-12-05 23:40:51 +0200

    Update task_1_20.java

--- task_1_20.java
+++ task_1_20.java
@@ -3,26 +3,6 @@ import java.util.*;
 public class task_1_20 {
     static Scanner sc = new Scanner(System.in);
 
-    // 1.10
-    static void task110(int[] array) {
-        String pal = "", temp, s;
-
-        for (int i : array) {
-            s = Integer.toString(i);
-            temp = new StringBuilder(s).reverse().toString();
-
-            if (s.equals(temp)) {
-                if (pal != "") {
-                    System.out.println(s);
-                    return;
-                } else pal = s;
-            }
-        }
-
-        System.out.println(pal);
-    }
-
-
     // 1.20
     static void task120() {
         int n = sc.nextInt();
