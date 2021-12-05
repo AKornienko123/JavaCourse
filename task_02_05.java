aaace9f566e7e601a8d866c58f8e6ba008c38919
AKornienko123 <71881413+AKornienko123@users.noreply.github.com>
2021-12-05 23:36:08 +0200

    Update task_02_05.java

--- task_02_05.java
+++ task_02_05.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class T_02_05 {
+public class task_02_05 {
 	public static void main(String[] args) {
 	    for( int i = args.length - 1 ; i >= 0 ; i--)
 	        System.out.println("Reverse : " + args[i]);
