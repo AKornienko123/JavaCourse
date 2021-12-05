4d12e015f9048319fd0207d3d87f821344bc1670
AKornienko123 <71881413+AKornienko123@users.noreply.github.com>
2021-12-05 21:02:02 +0200

    CW1

---
+++ Task_1_5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task_1_5 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		for (int i = 0; i < 3; i++) {
+			System.out.println(args[i]);	
+		}
+		in.close();
+	}
+}
+
